81dd70715e4d8fcb28a4c61007db2ade957e8c13
Alec Scully <ScullyAlec@gmail.com>
2021-04-14 16:22:34 -0400

    GoatTracker done

--- GoatTracker/index.js
+++ GoatTracker/index.js
@@ -8,3 +8,40 @@ const toph = {
 };
 //  Code goes below here!
 
+document.addEventListener('DOMContentLoaded', () => {
+  renderGoat()
+})
+
+const renderGoat = () => {
+  let goatName = document.querySelector('.goat-name')
+      goatName.innerText = toph.name;
+
+  let goatAge = document.querySelector('.goat-age')
+      goatAge.innerText = toph.age;
+
+  let goatBrushes = document.querySelector('.goat-brushes')
+      goatBrushes.innerHTML = toph.brushesToday;
+
+  let goatStatus = document.querySelector('.goat-status')
+      goatStatus.innerText = toph.badGoat;
+
+  let brushButton = document.querySelector('.brush-button')
+      brushButton.addEventListener('click', () => brushGoat())
+
+  let toggleGoat = document.querySelector('.goat-toggle')
+      toggleGoat.addEventListener('click', () => toggleStatus())
+}
+
+const brushGoat = () => {
+  let goatBrushes = document.querySelector('.goat-brushes')
+      goatBrushes.innerText = parseInt(goatBrushes.innerText) + 1
+}
+
+const toggleStatus = () => {
+  let goatStatus = document.querySelector('.goat-status')
+      if (goatStatus.innerText == "false") {
+        goatStatus.innerText = "true"
+      } else {
+        goatStatus.innerText = "false"
+      }
+}
